d8e95262602ba0aab861aa4c92e26a14cd96cab9
KevinG9800 <kevingarza980@gmail.com>
2023-04-05 21:02:15 -0600

    Creating the basic structure of the pages

---
+++ acceder_cuenta.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceder a cuenta</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/estilo01.css">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <button type="button" class="btn btn-outline-primary">Registrar cuenta  </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <button type="button" class="btn btn-outline-primary">Iniciar sesión</button>
+            </div>
+        </div>
+
+    </div>
+
+
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
